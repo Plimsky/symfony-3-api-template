f9bb195ffc25ac121417d4c12c6cc4af889d695f
Vincent Nicopolsky <vincent.nicopolsky@gmail.com>
2016-11-08 13:41:18 +0100

    Reorganize the config arch for better view folder

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle()
         ];
 
-        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+        if (in_array($this->getEnvironment(), ['dev',
+            'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
@@ -36,16 +37,30 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        if (in_array($this->getEnvironment(), [
+            'dev',
+            'test'
+        ])) {
+            return '/var/tmp/api/cache/' . $this->getEnvironment();
+        }
+
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        if (in_array($this->getEnvironment(), [
+            'dev',
+            'test'
+        ])) {
+            return '/var/tmp/api/logs/' . $this->getEnvironment();
+        }
+
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/' . $this->getEnvironment() . '/config_' . $this->getEnvironment() . '.yml');
     }
 }
